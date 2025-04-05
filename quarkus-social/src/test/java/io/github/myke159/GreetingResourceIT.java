7ab23557c154f1d5982ffb464e20abbc1b57f5cf
Myke Bueno <myke159951@hotmail.com>
2025-04-05 20:29:51 -0300

    create quarkus-social

---
+++ quarkus-social/src/test/java/io/github/myke159/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.myke159;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
